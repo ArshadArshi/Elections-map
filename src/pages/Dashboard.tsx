75117c97700319e6e5a46914a1b0edd68d0013a2
unknown <arshad@vibhotech.com>
2024-08-29 12:21:46 +0530

    first commit

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,38 +1,104 @@
-import DOMPurify from 'dompurify';
-import { useEffect, useState } from "react";
 import { PageHeader, PageHeaderHeading } from "@/components/page-header";
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
-import { Loader2 } from 'lucide-react';
-import { toast } from 'sonner';
-// import { loggedIn } from "@/Redux/reducers/login";
-
-// Define the data type for articles
-interface Article {
-  id: number;
-  title: string;
-  description: string;
-  content: string;
-}
+import JammuMap from "./JammuMap";
+import "./Main.css";
+import { useEffect, useState } from "react";
+import { FeatureCollection, GeoJsonProperties, Geometry } from "geojson";
+import geojsonData from "../../j_and_k_assembly_new_borders.json";
 
 export default function Dashboard() {
+  const [bjpCount, setBjpCount] = useState(0);
+  const [incCount, setIncCount] = useState(0);
+  const [congressCount, setCongressCount] = useState(0);
+  const [otherCount, setOtherCount] = useState(0);
 
+  const geojson: FeatureCollection<Geometry, GeoJsonProperties> =
+    geojsonData as FeatureCollection<Geometry, GeoJsonProperties>;
 
+  useEffect(() => {
+    // Count the number of features for each party
+    const bjpCount = geojson.features.filter(
+      (feature) => feature.properties?.party === "BJP"
+    ).length;
+
+    const incCount = geojson.features.filter(
+      (feature) => feature.properties?.party === "INC"
+    ).length;
+
+    // const congressCount = geojson.features.filter(
+    //   (feature) => feature.properties?.party === "CONGRESS" && ""
+    // ).length;
+
+    const otherCount =
+      geojson.features.filter((feature) => feature.properties?.party === "")
+        .length +
+      geojson.features.filter(
+        (feature) => feature.properties?.party === "CONGRESS"
+      ).length;
+
+    setBjpCount(bjpCount);
+    setIncCount(incCount);
+    setCongressCount(congressCount);
+    setOtherCount(otherCount);
+  }, [setBjpCount, setIncCount, setCongressCount, setOtherCount]);
 
   return (
     <>
       <PageHeader>
         <PageHeaderHeading>Articles</PageHeaderHeading>
       </PageHeader>
-      <div>
-        dashbord
+      <div style={{ display: "flex", zIndex: "5" }}>
+        <div>
+          <JammuMap
+            height="350px"
+            width="350px"
+            zoom={7}
+            bjpCount={0}
+            incCount={0}
+            congressCount={0}
+            otherCount={0}
+          />
+          <div className="count">
+            <div className="countone">
+              <div className="bjp">BJP</div>
+              <div className="bjp-count">{bjpCount}</div>
+            </div>
+            <div className="counttwo">
+              <div className="inc">INC</div>
+              <div className="inc-count">{incCount}</div>
+            </div>
+            {/* <div className="countothers">
+      <div className="other">CONGRESS</div>
+      <div className="other-count">{congressCount}</div>
+      </div> */}
+            <div className="countothers">
+              <div className="other">OTHERS</div>
+              <div className="other-count">{otherCount}</div>
+            </div>
+          </div>
+        </div>
+        {/* <JammuMap height="350px" width="350px" zoom={7} /> */}
+        {/* <JammuMap height="350px" width="350px" zoom={7} /> */}
+      </div>
+      <div
+        style={{
+          position: "fixed",
+          right: "20px",
+          top: "25%",
+          width: "200px",
+          height: "200px",
+          backgroundColor: "#9DBDFF",
+        }}
+      >
+        <p
+          style={{
+            width: "100%",
+            textAlign: "center",
+            fontSize: "20px",
+            color: "black",
+          }}
+        >
+          hi there
+        </p>
       </div>
     </>
   );
---
+++ src/pages/JammuMap.tsx
@@ -0,0 +1,266 @@
+import React, { useEffect, useState } from "react";
+import { MapContainer, GeoJSON, Marker, Tooltip, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import geojsonData from "../../j_and_k_assembly_new_borders.json";
+import {
+  FeatureCollection,
+  Feature,
+  Geometry,
+  GeoJsonProperties,
+} from "geojson";
+import L from "leaflet";
+// import markIcon from '../assets/mapmarker.png'
+
+interface Props {
+  height: string;
+  width: string;
+  zoom: number;
+  bjpCount: number;
+  incCount: number;
+  congressCount: number;
+  otherCount: number;
+}
+
+type DistrictKeys = {
+  dist: string;
+  party: string;
+  color: string;
+  opacity?: number;
+};
+
+const geojson: FeatureCollection<Geometry, GeoJsonProperties> =
+  geojsonData as FeatureCollection<Geometry, GeoJsonProperties>;
+
+const JammuMap: React.FC<Props> = ({ height, width, zoom }) => {
+  const [color, setColor] = useState<DistrictKeys[]>([]);
+  const [selected, setSelected] = useState<string | null>(null);
+  const [marker, setMarker] = useState<{
+    position: L.LatLngExpression;
+    popupText: string;
+    country: string;
+    imageUrl: string;
+  } | null>(null);
+  const [showPopup, setShowPopup] = useState(false);
+
+  useEffect(() => {
+    setColor((prev) => {
+      prev.map((item) => (item.opacity = 0.5));
+      return prev;
+    });
+  }, []);
+
+  const countParties = () => {
+    const counts = { BJP: 0, INC: 0, CONGRESS: 0, OTHERS: 0 };
+
+    geojson.features.forEach((feature: any) => {
+      const party = feature.properties.party;
+      if (party === "BJP") counts.BJP++;
+      else if (party === "INC") counts.INC++;
+      else if (party === "CONGRESS") counts.CONGRESS++;
+      else counts.OTHERS++;
+    });
+
+    return counts;
+  };
+
+  const partyCounts = countParties();
+
+  const highlightFeature = (e: L.LeafletMouseEvent) => {
+    const layer = e.target;
+    const district = layer.feature?.properties?.seat_district_en;
+    const country = layer.feature?.properties?.country;
+    const imageUrl = layer.feature?.properties?.imageUrl;
+
+    setColor((prev) => {
+      prev.map((item) => {
+        if (item.party === district) item.opacity = 1;
+        else item.opacity = 0.5;
+      });
+      return prev;
+    });
+    setSelected(district || ""); // Set the selected district
+    setMarker({
+      position: e.latlng as L.LatLngExpression,
+      popupText: district || "",
+      country: country,
+      imageUrl: imageUrl,
+    });
+    setShowPopup(true);
+  };
+
+  const handleMouseOver = () => {
+    setShowPopup(true);
+  };
+
+    const handleMouseOut = () => {
+      setShowPopup(false);
+    }
+
+  const styleFeature = (feature?: Feature<Geometry, GeoJsonProperties>) => {
+    if (!feature) return {};
+
+    const party = feature.properties?.party;
+    let fillColor;
+    if (party === "BJP") {
+      fillColor = "#FF9900";
+    } else if (party === "INC") {
+      fillColor = "#4DB6E2";
+    } else if (party === "CONGRESS") {
+      fillColor = "#006400";
+    } else {
+      fillColor = "#808080";
+    }
+
+    return {
+      fillColor: fillColor || "#a9a5a5",
+      color: "#000",
+      weight: 0.5,
+      fillOpacity: 1,
+      transition: "all 0.3s ease",
+    } as L.PathOptions;
+  };
+
+  const onEachFeature = (
+    feature: Feature<Geometry, GeoJsonProperties>,
+    layer: L.Layer
+  ) => {
+    if (feature.properties && feature.properties.seat_district_en) {
+      layer.on({
+        click: highlightFeature,
+      });
+    }
+  };
+
+    // const markerIcon = new L.Icon({
+    //   iconUrl: markIcon,
+    //   iconSize: [35, 45],
+    //   iconAnchor: [17, 46],
+    //   popupAnchor: [0, -46],
+    // })
+
+  const Legend = () => (
+    <div
+      style={{
+        position: "absolute",
+        bottom: "10px",
+        right: "230px",
+        // backgroundColor: "white",
+        padding: "10px",
+        // borderRadius: "5px",
+        // boxShadow: "0 0 15px rgba(0, 0, 0, 0.2)",
+        zIndex: 1000,
+        fontSize: "10px",
+      }}
+    >
+      <h4>NDA</h4>
+      <div className="legend-text">
+        <span
+          style={{
+            backgroundColor: "#FF9900",
+            width: "15px",
+            height: "15px",
+            display: "inline-block",
+          }}
+        ></span>
+        <span>BJP ({partyCounts.BJP})</span>
+      </div>
+      <h4>UPA</h4>
+      <div className="legend-text">
+        <span
+          style={{
+            backgroundColor: "#4DB6E2",
+            width: "15px",
+            height: "15px",
+            display: "inline-block",
+          }}
+        ></span>
+        <span>INC ({partyCounts.INC})</span>
+      </div>
+      <h4>OTHERS</h4>
+      <div className="legend-text">
+        <span
+          style={{
+            backgroundColor: "#006400",
+            width: "15px",
+            height: "15px",
+            display: "inline-block",
+          }}
+        ></span>
+        <span>CONGRESS ({partyCounts.CONGRESS})</span>
+      </div>
+      <div className="legend-text">
+        <span
+          style={{
+            backgroundColor: "#808080",
+            width: "15px",
+            height: "15px",
+            display: "inline-block",
+          }}
+        ></span>
+        <span>OTHERS ({partyCounts.OTHERS})</span>
+      </div>
+    </div>
+  );
+
+  return (
+    <div
+      style={{
+        height: height,
+        width: width,
+        position: "relative",
+        marginBottom: "10px",
+      }}
+    >
+      <MapContainer
+        center={[33.7, 75.01]}
+        zoom={zoom}
+        style={{
+          height: "100%",
+          width: "100%",
+          background: "none",
+          marginLeft: "110px",
+        }}
+        scrollWheelZoom={false}
+        keyboard={false}
+        // zoomControl={false}
+        // doubleClickZoom={false}
+        // dragging={false}
+        attributionControl={false}
+      >
+        <GeoJSON
+          data={geojson}
+          style={styleFeature}
+          onEachFeature={onEachFeature}
+        />
+        {marker?.position && (
+          <Marker
+            position={marker.position}
+            eventHandlers={{ mouseover: handleMouseOver, mouseout: handleMouseOut }}
+          >
+            <Tooltip permanent opacity={1}>
+              <strong>{selected}</strong>
+            </Tooltip>
+            {showPopup && (
+              <Popup>
+                <div>
+                  <img
+                    src={marker.imageUrl}
+                    alt={marker.popupText}
+                    style={{ width: "100px", height: "auto" }}
+                  />
+                  <br />
+                  <strong>{marker.popupText}</strong>
+                  <br />
+                  Country: {marker.country}
+                </div>
+              </Popup>
+            )}
+          </Marker>
+        )}
+      </MapContainer>
+      <Legend />
+    </div>
+  );
+};
+
+export default JammuMap;
